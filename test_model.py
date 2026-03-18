ec996969a076d8bea244ef0c738cd8fc583e9f2c
Gayathri Menon <gayathrikvrb@gmail.com>
2026-03-18 21:38:47 +0530

    trained and evaluated unmasked data as well

---
+++ test_model.py
@@ -0,0 +1,17 @@
+import timm
+import torch
+import torch.nn as nn
+
+num_classes = 3   
+
+model = timm.create_model('convit_tiny', pretrained=True)
+
+model.head = nn.Linear(model.head.in_features, num_classes)
+
+print(model.head)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = model.to(device)
+
+print(next(model.parameters()).device)
+
